9e0638386a3658ff06d26e769d6d808b4d6f81fd
cbmono <claudio@cbmono.de>
2011-03-21 11:38:00 +0100

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ebaydummy::Application.config.secret_token = 'b6711bcedb0059e534cd4fd30737fc8d32f14370e8d8d31e5875ed2e6e81d6193225c6088c6636f6d27052357190376e45d5b4ab7e5f66f03520b0cc6f32cab6'
